c432683bf9fdbe1818cca866bd27c4d620c383a0
Angela P. Cuervo-Robayo <ancuervo@gmail.com>
2019-02-01 21:18:53 -0600

    Update ajustarMask.R
    bueno

--- R/ajustarMask.R
+++ R/ajustarMask.R
@@ -1,16 +1,29 @@
 library(raster)
 setwd("C:/Proyectos/Wolke/Nuevo/Resultados_plantas_VaraiablesBaja")
+dir.create("mascara/")
 
 #Este codigo es para cortar un mapa de distribucion potencial continuo con el mapa binario correspondiente. 
 #Ademas lo ajusta a una misma mascara, dejandolo con el mismo numero de columnas y filas. 
 rm(list=ls())
 mask<-raster("C:/Proyectos/Wolke/mask.tif")
-#binarios <- Sys.glob("acutiacutifolius/*.tif$")
-binarios <- list.files("C:/Proyectos/Wolke/Nuevo/Resultados_plantas_VaraiablesBaja", pattern = "_q10_", full.names = T, recursive = T)
-binarios
+print(mask)
+newextent<-c(-118.375,-107.85,22.51271, 32.66271)#binarios <- Sys.glob("acutiacutifolius/*.tif$")
+binarios <- list.files("C:/Proyectos/Wolke/Nuevo/Resultados_plantas_VaraiablesBaja", 
+                       pattern = "_q10_", full.names = T, recursive = T)
 
-x<-binarios[[1]]
-dir.create("mascara/")
+binarios
+#x<-binarios[[1]]
+nuevoExt<-lapply(binarios, function(x){
+  mapa<-raster(x)
+  print(x)
+  sp_map<-extend(mapa, newextent)
+  final <- mask*sp_map
+  outname <- paste0("mascara/", basename(x))
+  png(filename=paste0(basename(x), ".png"))
+  plot(final, main = basename(x))
+  dev.off()
+  writeRaster(final, outname, overwrite = TRUE)
+})
 
 #####
 hacerLoQueQuiero <- lapply(binarios, function(x, mask){ 
@@ -27,6 +40,4 @@ hacerLoQueQuiero <- lapply(binarios, function(x, mask){
 })
 
 apply(binarios, 1, hacerLoQueQuiero, mask = mask)
-
-
 
