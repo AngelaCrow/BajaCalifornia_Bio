d7855a07b84d97b51a3cbc503ea71ebf47853369
Angela Cuervo <angelacuervo@Angelas-MacBook-Pro.local>
2019-02-02 21:15:21 -0600

    para correr en mac

--- R/ajustarMask.R
+++ R/ajustarMask.R
@@ -1,43 +1,29 @@
 library(raster)
-setwd("C:/Proyectos/Wolke/Nuevo/Resultados_plantas_VaraiablesBaja")
-dir.create("mascara/")
+#setwd("C:/Proyectos/Wolke/Nuevo/Resultados_plantas_VaraiablesBaja")
+setwd("~/Documents/Wolke/Nuevo/")
+dir.create("Resultados_plantas_VaraiablesBaja/mascara/")
 
 #Este codigo es para cortar un mapa de distribucion potencial continuo con el mapa binario correspondiente. 
 #Ademas lo ajusta a una misma mascara, dejandolo con el mismo numero de columnas y filas. 
 rm(list=ls())
-mask<-raster("C:/Proyectos/Wolke/mask.tif")
+#mask<-raster("C:/Proyectos/Wolke/mask.tif")
+mask<-raster("mask.tif")
 print(mask)
 newextent<-c(-118.375,-107.85,22.51271, 32.66271)#binarios <- Sys.glob("acutiacutifolius/*.tif$")
-binarios <- list.files("C:/Proyectos/Wolke/Nuevo/Resultados_plantas_VaraiablesBaja", 
+binarios <- list.files("Resultados_plantas_VaraiablesBaja", 
                        pattern = "_q10_", full.names = T, recursive = T)
 
 binarios
-#x<-binarios[[1]]
+#x<-binarios[[43]]
 nuevoExt<-lapply(binarios, function(x){
   mapa<-raster(x)
   print(x)
   sp_map<-extend(mapa, newextent)
+  sp_map[is.na(sp_map[])] <- 0
   final <- mask*sp_map
-  outname <- paste0("mascara/", basename(x))
+  outname <- paste0("Resultados_plantas_VaraiablesBaja/mascara/", basename(x))
   png(filename=paste0(basename(x), ".png"))
   plot(final, main = basename(x))
   dev.off()
   writeRaster(final, outname, overwrite = TRUE)
 })
-
-#####
-hacerLoQueQuiero <- lapply(binarios, function(x, mask){ 
-  u <- raster(x)
-  print(u)
-  #u<-resample(u, mask)
-  u[is.na(u[])] <- 0
-  final <- mask*u
-  NAvalue(final) <- 0
-  outname <- paste0("mascara", basename(x))
-  plot(final, main = basename(x))
-  
-  writeRaster(final, outname, overwrite = TRUE)
-})
-
-apply(binarios, 1, hacerLoQueQuiero, mask = mask)
-
