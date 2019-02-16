35d7241cc7641189bada42861bc3016079aa8add
ancuervo@gmail.com <ancuervo@gmail.com>
2019-02-16 08:17:04 -0600

    rutas

--- R/DataSpecies.R
+++ R/DataSpecies.R
@@ -1,16 +1,18 @@
-#library(dplyr)
+library(dplyr)
 
-dataFilePath <- '/Users/angelacuervo/Documents/Wolke/BajaCalifornia_Bio/Botany/Endemismos.csv'
-outputDataFolder <- '/Users/angelacuervo/Documents/Wolke/BajaCalifornia_Bio/Botany/data_species/'
+dataFilePath <- 'G:/Mi unidad/BajaCalifornia_Bio/Species_DB/Angela/Master Baja Botany Points.csv'
+outputDataFolder <- 'C:/Proyectos/Wolke/BajaCalifornia_Bio/Botany/data_species/'
 
-#especieData <- data.table::fread(dataFilePath, encoding = "Latin-1")
-#Endemismos<-filter(especieData, Endemismos == "1")
-#write_csv(Endemismos, "/Users/angelacuervo/Documents/Wolke/BajaCalifornia_Bio/Botany/Endemismos.csv")
+especieData <- data.table::fread(dataFilePath, encoding = "Latin-1")
+Endemismos<-filter(especieData, Endemic == "1")
+write.csv(Endemismos, "C:/Proyectos/Wolke/BajaCalifornia_Bio/Botany/data_species/Endemic.csv")
+
+dataFilePath <- 'C:/Proyectos/Wolke/BajaCalifornia_Bio/Botany/data_species/Endemic.csv'
 
 especieData[,
             data.table::fwrite(
               .SD,
               file.path(outputDataFolder,
-                        paste0(gsub('([[:punct:]])|\\s+','_', Scientific_name),
+                        paste0(gsub('([[:punct:]])|//s+','_', StudyPlant),
                                ".csv"))),
-            by = .(Scientific_name)]
+            by = .(StudyPlant)]
--- R/PlotOccsData.R
+++ R/PlotOccsData.R
@@ -1,17 +1,17 @@
-library(raster)
+library("raster")
 library("rgdal", quietly = TRUE)
 library("magrittr", quietly = TRUE)
 library("readr", quietly = TRUE)
 library("dplyr", quietly = TRUE)
 
-
-setwd("/Users/angelacuervo/Documents/Wolke/BajaCalifornia_Bio/Botany/data_species/")
-outputFolder <- '/Users/angelacuervo/Documents/Wolke/BajaCalifornia_Bio/Botany/data_species/'
-mask<-raster("~/Documents/Wolke/Nuevo/mask.tif")
-sp_list<-list.files("/Users/angelacuervo/Documents/Wolke/BajaCalifornia_Bio/Botany/data_species/", 
+setwd("C:/Proyectos/Wolke/BajaCalifornia_Bio/Botany/data_species/")
+outputFolder <- 'C:/Proyectos/Wolke/BajaCalifornia_Bio/Botany/SinDups_dataSpecies'
+output_lentgth<-"C:/Proyectos/Wolke/BajaCalifornia_Bio/Botany/length"
+mask<-raster("C:/Proyectos/Wolke/BajaCalifornia_Bio/mask.tif")
+sp_list<-list.files("C:/Proyectos/Wolke/BajaCalifornia_Bio/Botany/data_species/", 
                     pattern = ".csv$*")
 
-#x<-sp_list[1]
+#x<-sp_list[6]
 
 plots_sps<-lapply(sp_list, function(x){
   crs.wgs84 <- sp::CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
@@ -20,12 +20,15 @@ plots_sps<-lapply(sp_list, function(x){
   sp::proj4string(occsData) <- crs.wgs84
   occsData <- sp::remove.duplicates(occsData, zero=0.00833333333)
   print(x)
-  outname <- paste0("/Users/angelacuervo/Documents/Wolke/BajaCalifornia_Bio/Botany/data_specie/", basename(x))
-  png(filename=paste0(basename(x), ".png"))
-  plot(mask, col="grey",main = basename(x))
+  dups<-data.frame(length(occsData))
+  dups["Scientificname"] <-basename(x)%>%gsub(".csv","",.)
+  write.csv(dups, file.path(output_lentgth, paste0(basename(x))),
+            row.names = FALSE)
+    png(filename=paste0(basename(x)%>%gsub(".csv","",.), ".png"))
+  plot(mask, col="grey",main = basename(x)%>%gsub(".csv","",.))
   points(occsData$Longitude, occsData$Latitude, pch='o', col='blue', cex=1.1)
   dev.off()
   write.csv(cbind(occsData@data, coordinates(occsData)), 
-            file = file.path(outputFolder, paste0(basename(x),"_", "sinDups",".csv")),
+            file = file.path(outputFolder, paste0(basename(x))),
           row.names = FALSE)
 })
